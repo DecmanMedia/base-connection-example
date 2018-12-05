4b11cc714d9f53473ffc079b6740793df28cd45b
DecmanMedia <diego.martinez.12@sansano.usm.cl>
2018-12-05 07:13:26 -0300

    Retrofit y Glade
    Glado implementado y funcional.
    Retrofit implementacion incompleta.
    (Esta instanciado pero no se logro hacer la adaptacion del traspaso de datos a RecyclerView)
    Glade funciona con el metodo antiguo de HTTPconnetcion.

--- app/src/main/java/cl/telematica/basicconnectionexample/main/MyAsyncTask.java
+++ app/src/main/java/cl/telematica/basicconnectionexample/main/MyAsyncTask.java
@@ -2,9 +2,16 @@ package cl.telematica.basicconnectionexample.main;
 
 import android.os.AsyncTask;
 
+import java.util.List;
+
 import cl.telematica.basicconnectionexample.connection.HttpServerConnection;
+import cl.telematica.basicconnectionexample.connection.RetrofitConnection;
 import cl.telematica.basicconnectionexample.models.Libro;
 import cl.telematica.basicconnectionexample.main.view.MainView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class MyAsyncTask extends AsyncTask<Void, Void, String> {
 
@@ -26,15 +33,36 @@ public class MyAsyncTask extends AsyncTask<Void, Void, String> {
 
     @Override
     protected String doInBackground(Void... voids) {
-        String resultado = new HttpServerConnection().connectToServer(url, timeOut);
-        return resultado;
+
+
+        Retrofit restAdapter = new Retrofit.Builder()
+                .baseUrl("http://www.mocky.io/")
+                .build();
+
+        RetrofitConnection service = restAdapter.create(RetrofitConnection.class);
+        service.getLibro(new Callback<List<Libro>>() {
+
+            @Override
+            public void onResponse(Call<List<Libro>> call, Response<List<Libro>> response) {
+                List<Libro> list = response.body();
+                mainView.populateRecyclerView(list);
+            }
+
+            @Override
+            public void onFailure(Call<List<Libro>> call, Throwable t) {
+
+            }
+        });
+
+        //String resultado = new HttpServerConnection().connectToServer(url, timeOut);
+        return "Hola";
     }
 
     @Override
     protected void onPostExecute(String result) {
         if(result != null) {
             //populateRecyclerView(Libro.getLibros(result));
-            mainView.populateRecyclerView(Libro.getLibros(result));
+            //mainView.populateRecyclerView(Libro.getLibros(result));
         }
     }
 
